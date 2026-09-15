868a716a7608807c9aa548c7d1cb58ebc3da7bbd
khanarsh2324-bit <khanarsh2324@gmail.com>
2026-09-15 16:43:37 +0530

    Update and rename A2.5.py to A2.5(check for vowel).py

---
+++ A2.5(check for vowel).py
@@ -0,0 +1,6 @@
+print("A2.5")
+char = input("enter a character: ")      #takes input from user
+if char in "aeiou":                      #checka for weather char entered by user is in 'aeiou'
+    print("vowel")                       #if yes , then prints 'vowel'
+else:                                    #if no, prints 'consonent
+    print("consonent")
--- A2.5.py
+++
@@ -1,6 +0,0 @@
-print("A2.5")
-char = input("enter a character: ")
-if char in "aeiou":
-    print("vowel")
-else:
-    print("consonent")
