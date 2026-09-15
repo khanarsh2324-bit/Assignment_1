f7278d838331e1b32bb297050ca91de7c99045a2
khanarsh2324-bit <khanarsh2324@gmail.com>
2026-09-15 16:55:33 +0530

    Update and rename A2.8.py to A2.8 (check that number is btw 1-100).py

---
+++ A2.8 (check that number is btw 1-100).py
@@ -0,0 +1,7 @@
+print("A2.8")
+n = int(input("Enter Number:"))            #takes input from user(only integer)
+if 1<n<100:                                # checks that the input in between 1-100
+    print("Number is between 1-100")       # if yes , prints "Number is between 1-100" , if no,moves to next condition
+else:                                      
+    print("Number is not between 1-100")   # if yes , prints "Number is not between 1-100"
+    
--- A2.8.py
+++
@@ -1,7 +0,0 @@
-print("A2.8")
-n = int(input("Enter Number:"))
-if 1<n<100:
-    print("Number is between 1-100")
-else:
-    print("Number is not between 1-100")
-    
