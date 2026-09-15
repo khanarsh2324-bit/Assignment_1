c84a6c6a401cf05f86a0d93b2fc05b2e0feb2c0b
khanarsh2324-bit <khanarsh2324@gmail.com>
2026-09-15 16:38:15 +0530

    Update and rename A2.3.py to A2.3 (print twinkle-twinkle).py

--- A2.3 (print twinkle-twinkle).py
+++ A2.3 (print twinkle-twinkle).py
@@ -1,2 +1,3 @@
 print("A2.3")
 print("\tTwinkle, twinkle, little star,\n\t\tHow I wonder what you are!\n\t\t\t\tUp above the world so high,\n\t\t\t\tLike a diamond in the sky.")
+# \n--> to add new line , \t--> to add space in the same line (also called tab)
